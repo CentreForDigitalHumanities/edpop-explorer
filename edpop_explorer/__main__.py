c2992eed172955e6cb383fcecee6cd493b413936
Tijmen Baarda <tijmenbaarda@gmail.com>
2023-02-22 11:19:49 +0100

    Implement commands next and show

--- edpop_explorer/__main__.py
+++ edpop_explorer/__main__.py
@@ -1,16 +1,40 @@
-from typing import Dict
+from typing import Dict, List, Optional
 import readline
+import math
 
-from apireader import APIReader
+from apireader import APIReader, APIRecord
 from readers.hpb import HPBReader
 
 
 readercommands: Dict[str, APIReader] = {
-    'hpb': HPBReader
+    'hpb': HPBReader,
 }
 
 
-def main():
+def show_records(records: List[APIRecord],
+                 start: int,
+                 end: Optional[int] = None) -> None:
+    if len(records) == 0:
+        return
+    if end is None:
+        end = len(records)
+    digits = int(math.log10(len(records))) + 1
+    i = start
+    while i < end:
+        print('{:{digits}} - {}'.format(
+            i + 1, records[i].get_title(), digits=digits
+        ))
+        i += 1
+
+
+def main() -> None:
+    print(
+        'Welcome to the EDPOP explorer!\n'
+        'Available commands: {}, next, exit'
+        .format(', '.join(readercommands))
+    )
+    reader: APIReader = None
+    shown: int = 0
     while True:
         try:
             line = input('# ')
@@ -19,7 +43,7 @@ def main():
         except KeyboardInterrupt:
             print('')
             continue
-        if line:
+        if line.strip() != '':
             command, *arguments = line.split()
         else:
             continue
@@ -30,10 +54,33 @@ def main():
                 print('{} command expects one argument.'.format(command))
                 continue
             readerclass = readercommands[command]
+            # Invoke constructor of readerclass
             reader = readerclass()
+            shown = 0
             records = reader.fetch(arguments[0])
-            for record in records:
-                print(record)
+            print('{} records found.'.format(reader.number_of_results))
+            show_records(records, shown)
+            shown += len(records)
+        elif command == 'next':
+            if reader is None:
+                print('First perform an initial search')
+            else:
+                records = reader.fetch_next()
+                show_records(reader.records, shown)
+                shown += len(records)
+        elif command == 'show':
+            if reader is None:
+                print('First perform an initial search')
+                continue
+            try:
+                index = int(arguments[0]) - 1
+            except (IndexError, ValueError):
+                print('Please provide a valid number')
+                continue
+            try:
+                print(reader.records[index].show_record())
+            except IndexError:
+                print('Please provide a record number that has been loaded')
         else:
             print('Command does not exist: {}'.format(command))
 
--- edpop_explorer/apireader.py
+++ edpop_explorer/apireader.py
@@ -1,2 +1,26 @@
+from dataclasses import dataclass
+from typing import Optional, List
+
+
+@dataclass
+class APIRecord:
+    url: Optional[str] = None
+
+    def get_title(self) -> str:
+        '''Convenience method to retrieve the title of a record in a standard
+        way'''
+        raise NotImplementedError('Should be implemented by subclass')
+
+    def show_record(self) -> str:
+        raise NotImplementedError('Should be implemented by subclass')
+
+
 class APIReader:
-    pass
+    number_of_results: int = None
+    records: List[APIRecord]
+
+    def fetch(self, query: str) -> List[APIRecord]:
+        raise NotImplementedError('Should be implemented by subclass')
+
+    def fetch_next(self) -> List[APIRecord]:
+        raise NotImplementedError('Should be implemented by subclass')
