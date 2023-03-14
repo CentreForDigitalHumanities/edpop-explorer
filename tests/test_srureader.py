924b86d5f4094f085effd0d1ca1c18bb28a588b7
Tijmen Baarda <tijmenbaarda@gmail.com>
2023-03-14 15:30:00 +0100

    Update test

--- tests/test_srureader.py
+++ tests/test_srureader.py
@@ -3,7 +3,7 @@ from unittest.mock import patch
 import json
 from pathlib import Path
 
-from edpop_explorer.srureader import SRUReader
+from edpop_explorer.srumarc21reader import SRUMarc21Reader
 
 
 TESTDATA = json.load(open(Path(__file__).parent / 'TESTDATA', 'r'))
@@ -13,10 +13,14 @@ class SRUReaderTestCase(unittest.TestCase):
     @patch('edpop_explorer.srureader.sruthi')
     def test_fetch(self, mock_sruthi):
         mock_sruthi.searchretrieve.return_value = TESTDATA
-        reader = SRUReader()
+        reader = SRUMarc21Reader()
+        reader.transform_query = lambda x: x
+        reader.get_link = lambda x: x
         reader.sru_url = ''
         reader.sru_version = '1.1'
-        results = reader.fetch('testquery')
+        reader.prepare_query('testquery')
+        reader.fetch()
+        results = reader.records
         # Field with multiple subfields
         self.assertEqual(
             results[0].get_first_field('245').subfields['a'],
