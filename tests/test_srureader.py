677efa7b0861fb486d216c352f7847e1efd34888
Tijmen Baarda <tijmenbaarda@gmail.com>
2023-03-21 15:36:04 +0100

    Convert to pytest

--- tests/test_srureader.py
+++ tests/test_srureader.py
@@ -9,7 +9,7 @@ from edpop_explorer.srumarc21reader import SRUMarc21Reader
 TESTDATA = json.load(open(Path(__file__).parent / 'TESTDATA', 'r'))
 
 
-class SRUReaderTestCase(unittest.TestCase):
+class TestSRUReader:
     @patch('edpop_explorer.srureader.sruthi')
     def test_fetch(self, mock_sruthi):
         mock_sruthi.searchretrieve.return_value = TESTDATA
@@ -22,22 +22,13 @@ class SRUReaderTestCase(unittest.TestCase):
         reader.fetch()
         results = reader.records
         # Field with multiple subfields
-        self.assertEqual(
-            results[0].get_first_field('245').subfields['a'],
+        assert results[0].get_first_field('245').subfields['a'] == \
             'Aeschylus: Eumenides.'
-        )
         # Field with a single subfield
-        self.assertEqual(
-            results[0].get_first_field('650').subfields['a'],
+        assert results[0].get_first_field('650').subfields['a'] == \
             'Aeschylus Eumenides.'
-        )
         # Field's description
-        self.assertEqual(
-            results[0].get_first_field('650').description,
+        assert results[0].get_first_field('650').description == \
             'Subject Added Entry - Topical Term'
-        )
         # Field that occurs multiple times
-        self.assertEqual(
-            len(results[0].get_fields('500')),
-            5
-        )
+        assert len(results[0].get_fields('500')) == 5
