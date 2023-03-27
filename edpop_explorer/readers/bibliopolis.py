828f012032f27c941244dc30e58b96993e532e22
Tijmen Baarda <tijmenbaarda@gmail.com>
2023-03-27 10:05:04 +0200

    Remove redundant get_link method

--- edpop_explorer/readers/bibliopolis.py
+++ edpop_explorer/readers/bibliopolis.py
@@ -41,18 +41,3 @@ class BibliopolisReader(SRUReader):
 
     def transform_query(self, query: str) -> str:
         return query
-
-    def get_link(self, record: APIRecord):
-        return None
-        # The record id can be found in field 035 in subfield a starting
-        # with (CERL), like this: (CERL)HU-SzSEK.01.bibJAT603188.
-        # The URI can then be created using HPB_URI.
-        # HPB records have field 035 two times.
-        fields035 = record.get_fields('035')
-        for field in fields035:
-            if 'a' in field.subfields and \
-                    field.subfields['a'].startswith('(CERL)'):
-                return self.HPB_LINK.format(
-                    field.subfields['a'][len('(CERL)'):]
-                )
-        return None
