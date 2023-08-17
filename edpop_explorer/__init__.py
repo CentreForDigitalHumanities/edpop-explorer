ab3b40943837fe86fe87b49ad112a281f902767c
Tijmen Baarda <tijmenbaarda@gmail.com>
2023-08-17 13:35:19 +0200

    Simplify package imports

--- edpop_explorer/__init__.py
+++ edpop_explorer/__init__.py
@@ -1,3 +1,10 @@
 from .rdf import EDPOPREC, RELATORS
-
-__all__ = [EDPOPREC, RELATORS]
+from .fields import Field, FieldError
+from .apireader import (
+    APIReader, APIRecord, RawData, APIException, BibliographicalRecord
+)
+from .srureader import SRUReader
+from .srumarc21reader import (
+    Marc21Data, Marc21Field, Marc21BibliographicalRecord, Marc21DataMixin,
+    SRUMarc21Reader, SRUMarc21BibliographicalReader
+)
