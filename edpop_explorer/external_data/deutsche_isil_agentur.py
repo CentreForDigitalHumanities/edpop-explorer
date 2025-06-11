1dc6d683b77e326383bfc396c99bbac21550661d
Tijmen Baarda <tijmenbaarda@gmail.com>
2025-06-11 13:02:04 +0200

    Fix for Python 3.9 support

--- edpop_explorer/external_data/deutsche_isil_agentur.py
+++ edpop_explorer/external_data/deutsche_isil_agentur.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from urllib.error import HTTPError
 
 from rdflib import Graph, URIRef, Namespace
@@ -11,7 +12,7 @@ def get_isil_uri(code: str) -> str:
     return f"https://ld.zdb-services.de/resource/organisations/{code}"
 
 
-def get_isil_name_by_code(code: str) -> str | None:
+def get_isil_name_by_code(code: str) -> Optional[str]:
     """Get the short name of an institution from the data of Deutsche ISIL
     Agentur. Return None if the code is not found."""
     if code in _code_to_name:
