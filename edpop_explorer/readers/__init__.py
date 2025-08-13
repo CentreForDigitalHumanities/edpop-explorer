63cbe2151f6077116701cc902477d37834c48a62
Tijmen Baarda <tijmenbaarda@gmail.com>
2025-08-13 15:29:00 +0200

    Add convenience function to get a record by its URI

--- edpop_explorer/readers/__init__.py
+++ edpop_explorer/readers/__init__.py
@@ -20,6 +20,7 @@ __all__ = [
     "DutchAlmanacsReader",
     "PierreBelleReader",
     "ESTCReader",
+    "get_record_by_uri",
     "ALL_READERS",
 ]
 
@@ -39,6 +40,7 @@ from .kvcs import KVCSReader
 from .dutch_almanacs import DutchAlmanacsReader
 from .pierre_belle import PierreBelleReader
 from .estc import ESTCReader
+from .utils import get_record_by_uri
 
 import sys
 from typing import List, Type
@@ -47,11 +49,10 @@ from typing import List, Type
 def _get_all_readers() -> List[Type[Reader]]:
     """Create a list of all reader classes included in this package."""
     all_names = __all__.copy()
-    all_names.pop()  # Remove "ALL_READERS" itself
     all_readers: List[Type[Reader]] = []
     for name in all_names:
         cls = getattr(sys.modules[__name__], name, None)
-        if cls is not None:
+        if isinstance(cls, type) and issubclass(cls, Reader):
             all_readers.append(cls)
     return all_readers
 
